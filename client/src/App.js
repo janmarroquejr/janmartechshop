564e223ae529884307475c06f5958c70dcde8f60
Janmar Jose D. Roque Jr <janmarroquejr@gmail.com>
2020-10-05 13:19:39 +0800

    added dummy data to homescreen

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome to Janmar's Tech Shop!</h1>
-    </>
+    <Router>
+      <Header />
+      <main>
+        <Container>
+          <Route exact path="/" component={HomeScreen} />
+          <Route exact path="/product/:id" component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 };
 
