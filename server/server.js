7a513828e90ae2f23decd2a98e4aed27a42481f8
Janmar Jose D. Roque Jr <janmarroquejr@gmail.com>
2020-10-08 15:01:46 +0800

    added back end user auth

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
@@ -11,10 +12,12 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
-
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
