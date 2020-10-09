e5987bf6923be4375c5f6c2dffeda1a2e5fc6c87
Janmar Jose D. Roque Jr <janmarroquejr@gmail.com>
2020-10-09 08:14:31 +0800

    added login/logout functions

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const App = () => {
   return (
@@ -13,9 +14,10 @@ const App = () => {
       <Header />
       <main>
         <Container>
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/login" component={LoginScreen} />
           <Route exact path="/" component={HomeScreen} />
-          <Route exact path="/product/:id" component={ProductScreen} />
-          <Route exact path="/cart/:id?" component={CartScreen} />
         </Container>
       </main>
       <Footer />
