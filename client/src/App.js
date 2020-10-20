fbcb6481897a514f8b93b8901381c33423be84e9
Janmar Jose D. Roque Jr <janmarroquejr@gmail.com>
2020-10-20 15:53:03 +0800

    added admin middleware and userlist

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
+import UserListScreen from "./screens/UserListScreen";
 
 const App = () => {
   return (
@@ -29,6 +30,7 @@ const App = () => {
           <Route path="/cart/:id?" component={CartScreen} />
           <Route path="/login" component={LoginScreen} />
           <Route path="/register" component={RegisterScreen} />
+          <Route path="/admin/userlist" component={UserListScreen} />
           <Route exact path="/" component={HomeScreen} />
         </Container>
       </main>
